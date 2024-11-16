9e0000920e6584ace01080135a07a751acaa2342
Om Panpatil <ompanpatil513@gmail.com>
2024-11-16 14:45:52 +0530

    Js Concepts in mern Web Development

---
+++ IMPORTANTS/Learning_Js.js
@@ -0,0 +1,34 @@
+// js the language :
+    // (1) Basics - linking file, consoles,
+    // (2) Inbuilt features : browser alert, prompt, alternate to console.log, utility functions,
+    // (3) Variables and constants,
+    //  (4) Compilers and interpreters, 
+    // (5)Window,
+    // (6)Types, 
+    // (7)Conditional statements, 
+    // (8)Loops,
+    // (9)Functions,
+    // (10)Arrays,
+    // (11)Objects,
+    // (12)Prototypes,
+    // (13)Prototypal inheritance,
+    // (14) Asynchronous Javascript,
+    // (15)Es6 climax
+    // (16)Questions
+
+    // js the DOM
+
+
+    // Js the language :
+    //  - Developed by branden Eig in 20 days.
+    //  - That's why it has some difficulties so a team was made to resolve it and now also they are working.
+    //  - It is known as Ecma Script (ES)
+    // Js = Language
+    // Managed by ecmascript
+    // Naye features, puraane hattane, kharab features sahi karna, and overall development of js.
+    // Js the language : codelikhna seekhna, main features.
+    // Js the DOM : code likhkar cheezein banana seekhna
+
+    // Console, alert, prompt are the browsers tool.
+    // Console.log, console.warn, console.error
+    // prompt alert ka bhai hai jo ki alert se ek kadam agae hai
---
+++ Javascript/Basics_js/basics.js
@@ -0,0 +1,7 @@
+// Console, alert, prompt.
+// console.log
+console.log("Om");
+console.warn("Om");
+console.error("Om");
+alert("Heyy");
+prompt("Wassup ?");
